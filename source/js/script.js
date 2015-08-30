e33836e9f92f77dfc6cdd506513340669fcac3db
Austen <austenezzell@gmail.com>
2015-08-29 17:31:49 -0700

    first push

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+  (function($, window, document) {
+
+    function main() {
+      console.log('Hello, World!');
+    }
+
+    main();
+
+  } (window.jQuery, window, document));
